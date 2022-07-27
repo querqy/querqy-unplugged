3b6adbb83acb1dc9ef384f8412525833fc79eb21
JohannesDaniel <johannesdaniel.peter@gmx.de>
2022-07-27 13:24:42 +0200

    Implemented RawQueryConverter

---
+++ library/src/main/java/querqy/converter/map/RawQueryConverter.java
@@ -0,0 +1,21 @@
+package querqy.converter.map;
+
+
+import lombok.RequiredArgsConstructor;
+import querqy.model.RawQuery;
+import querqy.model.StringRawQuery;
+
+@RequiredArgsConstructor(staticName = "of")
+public class RawQueryConverter {
+
+    private final RawQuery rawQuery;
+
+    public String convert() {
+        if (rawQuery instanceof StringRawQuery) {
+            return ((StringRawQuery) rawQuery).getQueryString();
+
+        } else {
+            throw new UnsupportedOperationException("Conversion of RawQuery is only supported for type StringRawQuery");
+        }
+    }
+}
